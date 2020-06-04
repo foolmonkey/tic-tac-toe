514cbcff4c727286cce0dba619a998abecf4b15f
Andy Tran <foolmonkey99@gmail.com>
2020-06-04 16:48:46 -0600

    Add clickable game tile elements

--- script.js
+++ script.js
@@ -1,44 +1,78 @@
+// player factory
+const player = (name, marker) => {
+  const setName = (aName) => {
+    name = aName;
+  };
+
+  return () => {
+    name, marker, setName;
+  };
+};
+
 // gameboard module
 const gameboard = (() => {
-    let board = new Array(9);
+  let board = new Array(9);
+  let currentMarker = "X";
 
-    board[0] = "X";
-    board[4] = "X";
-    board[8] = "O";
-    return {
-        board
-    };
+  const addMarker = (index) => {
+    if (board[index] == null) {
+      board[index] = currentMarker;
+      toggleMarker();
+    }
+  };
+
+  const clear = () => {
+    board.splice(0, board.length);
+  };
+
+  const toggleMarker = () => {
+      if(currentMarker == "X"){
+          currentMarker = "O";
+      } else{
+          currentMarker = "X";
+      }
+  };
+
+  return {
+    board,
+    addMarker,
+    clear,
+  };
 })();
-
-// e.g. gameboard.add(3, 5) // 8
 
 // displayController module
 const displayController = (() => {
-    const render = () => {
-        let container = document.getElementById("container");
-        
-        for(let i = 0, len = gameboard.board.length; i < len; i++){
-            let square = document.createElement('div');
-            square.textContent = gameboard.board[i];
-            square.classList.add('square');
-            container.appendChild(square);
-        }
-    };
+  const render = () => {
+    let container = document.getElementById("container");
+    container.innerHTML = "";
 
-    return {
-        render
-    };
+    createGameTiles(container);
+  };
+
+  // append gameboard elements to container
+  const createGameTiles = (container) => {
+    for (let i = 0, len = gameboard.board.length; i < len; i++) {
+        let gameTile = document.createElement("div");
+        gameTile.textContent = gameboard.board[i];
+        gameTile.classList.add("square");
+        container.appendChild(gameTile);
+        
+        addGameTileListener(gameTile, i);
+      }
+  };
+
+  const addGameTileListener = (gameTile, index) => {
+    gameTile.addEventListener('click', function(){
+        gameboard.addMarker(index, "X");
+        gameTile.textContent = gameboard.board[index];
+    });
+  };
+
+  return {
+    render,
+  };
 })();
 
-// player factory
-const player = () => {
-    let _privVariable = 0;
-    let publicVariable = 0;
-    return () => {
-        publicVariable;
-    };
-};
-
-window.onload = function(){
-    displayController.render();
+window.onload = function () {
+  displayController.render();
 };
