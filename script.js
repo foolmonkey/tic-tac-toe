bafabcdad0a80c4c2568de9cf567f95fba3270b4
Andy Tran <foolmonkey99@gmail.com>
2020-06-04 17:59:03 -0600

    Add game win and tie logic

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ const player = (name, marker) => {
 const gameboard = (() => {
   let board = new Array(9);
   let currentMarker = "X";
+  let turn = 1;
+  let gameWon = "";
 
   const addMarker = (index) => {
-    if (board[index] == null) {
+    if (board[index] == null && !isGameOver()) {
       board[index] = currentMarker;
-      toggleMarker();
+      checkMove();
     }
   };
 
@@ -26,17 +28,75 @@ const gameboard = (() => {
   };
 
   const toggleMarker = () => {
-      if(currentMarker == "X"){
-          currentMarker = "O";
-      } else{
-          currentMarker = "X";
+    if (currentMarker == "X") {
+      currentMarker = "O";
+    } else {
+      currentMarker = "X";
+      turn++;
+    }
+  };
+
+  const checkMove = () => {
+    // check for tie
+    if (turn == 5) {
+      tieMove();
+    }
+
+    // check for winning move
+    if (turn >= 3) {
+      // check center of board
+      if (board[4] == currentMarker) {
+        if (board[0] == currentMarker && board[8] == currentMarker) {
+          winningMove();
+        } else if (board[2] == currentMarker && board[6] == currentMarker) {
+          winningMove();
+        } else if (board[1] == currentMarker && board[7] == currentMarker) {
+          winningMove();
+        } else if (board[3] == currentMarker && board[5] == currentMarker) {
+          winningMove();
+        }
       }
+
+      // check top-left corner
+      if (board[0] == currentMarker) {
+        if (board[1] == currentMarker && board[2] == currentMarker) {
+          winningMove();
+        } else if (board[3] == currentMarker && board[6] == currentMarker) {
+          winningMove();
+        }
+      }
+
+      // check bottom-right corner
+      if (board[8] == currentMarker) {
+        if (board[7] == currentMarker && board[6] == currentMarker) {
+          winningMove();
+        } else if (board[5] == currentMarker && board[2] == currentMarker) {
+          winningMove();
+        }
+      }
+    }
+    toggleMarker();
+  };
+
+  const winningMove = () => {
+    gameWon = currentMarker;
+    console.log(currentMarker + " Wins!" + " gamewon= " + gameWon);
+  };
+
+  const tieMove = () => {
+    gameWon = "T";
+    console.log("Tie!");
+  };
+
+  const isGameOver = () => {
+      return gameWon != '';
   };
 
   return {
     board,
     addMarker,
     clear,
+    isGameOver,
   };
 })();
 
@@ -52,19 +112,19 @@ const displayController = (() => {
   // append gameboard elements to container
   const createGameTiles = (container) => {
     for (let i = 0, len = gameboard.board.length; i < len; i++) {
-        let gameTile = document.createElement("div");
-        gameTile.textContent = gameboard.board[i];
-        gameTile.classList.add("square");
-        container.appendChild(gameTile);
-        
-        addGameTileListener(gameTile, i);
-      }
+      let gameTile = document.createElement("div");
+      gameTile.textContent = gameboard.board[i];
+      gameTile.classList.add("square");
+      container.appendChild(gameTile);
+
+      addGameTileListener(gameTile, i);
+    }
   };
 
   const addGameTileListener = (gameTile, index) => {
-    gameTile.addEventListener('click', function(){
-        gameboard.addMarker(index, "X");
-        gameTile.textContent = gameboard.board[index];
+    gameTile.addEventListener("click", function () {
+      gameboard.addMarker(index);
+      gameTile.textContent = gameboard.board[index];
     });
   };
 
